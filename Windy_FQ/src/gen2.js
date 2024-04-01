1665134fe76112cb9023ae9ae2b2868288b955f3
Inori-2607 <gamingytzeno@gmail.com>
2024-04-01 16:59:41 +0700

    z

--- Windy_FQ/src/gen2.js
+++ Windy_FQ/src/gen2.js
@@ -1,26 +1,24 @@
 load('config.js');
-
-function execute(url) {
-    let newurl = `https://novel.snssdk.com/api/novel/book/directory/detail/v1/?aid=1967&item_ids=${url}`
-    console.log(newurl)
-    
-	let response = fetch(newurl, {
-        headers: {
-            'user-agent': UserAgent.android()
-        }
-    });
+function execute(url, page) {
+    if (!page) page = Math.floor(Math.random() * 3) + 1
+    url = url.replace('{{page}}', page);
+    let response = fetch(config_host2 + url);
     if (response.ok) {
-        let res_json = response.json();
-        let item = res_json.data;
-        const book = [];
-        for (let i = 0; i < item.length; i ++) {
-            book.push({
-                name: item[i].title,           
-                url: config_host + "/reader/" + item[i].item_id,
+        let doc = response.json();
+        let rows = doc.data.data.book_info
+        const data = [];
+        rows.forEach(e => {
+            data.push({
+                name: e.book_name,
+                link: config_host + "/info?book_id=" + e.book_id,
+                cover: e.thumb_url,
+                description: e.author,
                 host: config_host
             })
-        }
-        return Response.success(book);  
+        });
+        let next = parseInt(page, 10) + 1
+        return Response.success(data, next.toString());
     }
     return null;
+
 }
