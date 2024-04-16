9a4dcf7271ad261ce8993b9fd1b260787cdd3d75
Inori-2607 <gamingytzeno@gmail.com>
2024-04-16 22:06:03 +0700

    try old

--- Windy_TEST/src/chap.js
+++ Windy_TEST/src/chap.js
@@ -1,3 +1,4 @@
+/*
 load('config.js');
 
 function execute(url) {
@@ -13,6 +14,27 @@ function execute(url) {
         let chapter_info = json.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
         chapter_info = Html.parse(chapter_info)
         chapter_info = chapter_info.select("article").html()
+        return Response.success(chapter_info);
+    }
+    return Response.error("Kiểm tra lại app Fanqie");
+
+
+
+
+}
+*/
+load('config.js');
+
+function execute(url) {
+    const regex = /(?:item_id=|\/)(\d+)$/;
+    let chapid = url.match(regex)[1]
+
+    url = "https://fq.beitai.vip" + "/content?item_id=" + chapid
+
+    let response_chapter_info = fetch(url)
+    if (response_chapter_info.ok) {
+        let json = response_chapter_info.json();
+        let chapter_info = json.data.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
         return Response.success(chapter_info);
     }
     return Response.error("Kiểm tra lại app Fanqie");
