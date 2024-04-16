bfc5645fe8d08dc49e8b1660f9515e274b7ec93a
Inori-2607 <gamingytzeno@gmail.com>
2024-04-16 22:11:03 +0700

    oldddd

--- Windy_TEST/src/chap.js
+++ Windy_TEST/src/chap.js
@@ -1,28 +1,3 @@
-/*
-load('config.js');
-
-function execute(url) {
-    const regex = /(?:item_id=|\/)(\d+)$/;
-    let chapid = url.match(regex)[1]
-
-    url = "https://api.mgz6.cc/content/" + chapid
-
-    let response_chapter_info = fetch(url)
-    if (response_chapter_info.ok) {
-        let json = response_chapter_info.json();
-
-        let chapter_info = json.data.content.replace(/<br\s*\/?>|\n/g, "<br><br>");
-        chapter_info = Html.parse(chapter_info)
-        chapter_info = chapter_info.select("article").html()
-        return Response.success(chapter_info);
-    }
-    return Response.error("Kiểm tra lại app Fanqie");
-
-
-
-
-}
-*/
 load('config.js');
 
 function execute(url) {
