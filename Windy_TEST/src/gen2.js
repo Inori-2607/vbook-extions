c268404fd47c63db77b1c7f20a87eca034426b08
Inori-2607 <gamingytzeno@gmail.com>
2024-04-23 06:30:52 +0700

    FIX

--- Windy_TEST/src/gen2.js
+++ Windy_TEST/src/gen2.js
@@ -12,7 +12,7 @@ function execute(url, page) {
                 name: e.book_name,
                 link: "https://fanqienovel.com" + "/page/" + e.book_id,
                 cover: e.thumb_url,
-                description: `${e.score} - ${e.last_chapter_title} `,
+                description: `Điểm : ${e.score}`,
                 host: config_host
             })
         });
